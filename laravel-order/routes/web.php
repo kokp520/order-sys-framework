7723b031320ece4f06e528c29ef13db5862622ce
adi.wu <adi.wu@kiwigame.com>
2025-04-20 13:09:43 +0800

    add config, route

--- laravel-order/routes/web.php
+++ laravel-order/routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\OrderController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// group, order 
+Route::group(['prefix' => 'order'], function () {
+    Route::get('/', [OrderController::class, 'index']);
+    Route::post('/', [OrderController::class, 'store']);
+    Route::get('/{order}', [OrderController::class, 'show']);
+    Route::put('/{order}', [OrderController::class, 'update']);
+    Route::delete('/{order}', [OrderController::class, 'destroy']);
 });
